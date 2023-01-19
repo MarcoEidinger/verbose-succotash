5437b48f40a89b12b61ea4913a8e6fa53b5e559c
Marco Eidinger <eidingermarco@gmail.com>
2023-01-18 20:14:46 -0800

    add

---
+++ src/Frameworks/Sources/Frameworks/Frameworks.swift
@@ -0,0 +1,6 @@
+public struct Frameworks {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ src/Frameworks/Tests/FrameworksTests/FrameworksTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Frameworks
+
+final class FrameworksTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Frameworks().text, "Hello, World!")
+    }
+}
